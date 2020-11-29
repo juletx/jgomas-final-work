e80cdf7350a40375d64aff6bb6f4c821d1fd0991
Julen Etxaniz <juletxara@gmail.com>
2020-11-29 22:48:21 +0100

    Remove unnecessary files

--- JGomas-Fighters/jgomas/bin/mas/jgomas_manager.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -classpath "lib/jade.jar:lib/jadeTools.jar:lib/Base64.jar:lib/http.jar:lib/iiop.jar:lib/beangenerator.jar:lib/jgomas.jar:lib/jason.jar:lib/JasonJGomas.jar:classes:." jade.Boot -gui "Manager:es.upv.dsic.gti_ia.jgomas.CManager(6,map_04,125,10)"
--- JGomas-Fighters/run-noGUI.bat
+++
@@ -1,6 +0,0 @@
-@cd /D %~dp0
-@cd jgomas/bin/mas
-start /B jgomas_manager.bat
-@timeout 5
-start /B jgomas_launcher.bat
-@cd ../render/w32
